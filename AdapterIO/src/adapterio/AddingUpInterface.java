09780fd5bdae396f64347ef9bc0edb1a5f9efbd7
ShirkeJR <jakub.rycy@outlook.com>
2016-05-20 13:52:17 +0200

    TEST

--- AdapterIO/src/adapterio/AddingUpInterface.java
+++ AdapterIO/src/adapterio/AddingUpInterface.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package adapterio;
 
 /**
  *
- * @author PiK
+ * @author ShirkeJR
  */
 public interface AddingUpInterface {
     public int AddTwoBinaryNumbers(int a, int b);
